5e547619332f9a2c148542c72180a668bbd23dcc
jdelaete <joseph.delaeter@gmail.com>
2018-10-18 13:27:08 +0200

    First Commit

---
+++ POOA_TP2_Drawing_JavaFx/src/drawing/StatutBar.java
@@ -0,0 +1,26 @@
+package drawing;
+
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+
+import java.util.Observer;
+
+
+public class StatutBar extends HBox implements drawing.Observer {
+    private Label labelForm;
+    private DrawingPane dp;
+
+    public StatutBar(DrawingPane drawingPane) {
+        this.dp = drawingPane;
+        this.labelForm = new Label("0 shapes(s)");
+        this.getChildren().addAll(this.labelForm);
+    }
+
+    public StatutBar() {
+
+    }
+
+    public void update () {
+        this.labelForm.setText(this.dp.getNumberShape() + " shape(s)");
+    }
+}

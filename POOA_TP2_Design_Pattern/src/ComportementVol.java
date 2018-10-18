f16cb231dc426a95ba273c6cac1e986a22478c84
jdelaete <joseph.delaeter@gmail.com>
2018-10-18 13:36:29 +0200

    ajout du tp2

---
+++ POOA_TP2_Design_Pattern/src/ComportementVol.java
@@ -0,0 +1,4 @@
+public interface
+ComportementVol {
+    public void voler();
+}
---
+++ POOA_TP2_Design_Pattern/src/MiniSimulateur.java
@@ -0,0 +1,7 @@
+public class MiniSimulateur {
+    public static void main(String[] args) {
+        Canard colvert = new Colvert();
+        colvert.effectuerCancan();
+        colvert.effectuerVol();
+    }
+}composite
